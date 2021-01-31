63ef62cb4b4dfeaf569d64811e1bd806b82389cd
Sergio Villagomez <43839357+smavo@users.noreply.github.com>
2021-01-31 16:45:56 -0500

    Add Components [Login NuevaCuenta], update files

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Login from './Components/auth/Login';
+import NuevaCuenta from './Components/auth/NuevaCuenta';
+import Proyectos from './Components/proyectos/Proyectos';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Build Project MERN 🚀</h2>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Login} />
+        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
+        <Route exact path="/proyectos" component={Proyectos} />
+      </Switch>
+    </Router>
   );
 }
 
