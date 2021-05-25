1d3ad139052bdf3840bd7bfae2b986dba0b5fe45
Sergio Villagomez <43839357+smavo@users.noreply.github.com>
2021-05-25 14:22:22 -0500

    chore: Add template [Notus-React]

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,21 +1,40 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import Login from './Components/auth/Login';
-import NuevaCuenta from './Components/auth/NuevaCuenta';
-import Proyectos from './Components/proyectos/Proyectos';
+import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "assets/styles/tailwind.css";
+
+// layouts
+
+import Admin from "layouts/Admin.js";
+import Auth from "layouts/Auth.js";
+import Proyectos from 'layouts/Proyectos'
+import Index from 'views/Index';
+
+// views without layouts
+// import Landing from "views/Landing.js";
+// import Profile from "views/Profile.js";
 
 function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Login} />
-        <Route exact path="/nueva-cuenta" component={NuevaCuenta} />
-        <Route exact path="/proyectos" component={Proyectos} />
-      </Switch>
-    </Router>
-  );
-}
+    return (
+        <Router>
+        <Switch>
+            {/* add routes with layouts */}
+            <Route path="/auth" component={Auth} />
+            <Route path="/admin" component={Admin} />
+            <Route path="/project" component={Proyectos} />
+            <Route path="/" exact component={Index} /> 
+
+            {/* add routes without layouts */}
+            {/* <Route path="/landing" exact component={Landing} />
+            <Route path="/profile" exact component={Profile} /> */}
+            {/* add redirect for first page */}
+            <Redirect from="*" to="/" />
+        </Switch>
+        </Router>
+    );
+    }
 
 export default App;
