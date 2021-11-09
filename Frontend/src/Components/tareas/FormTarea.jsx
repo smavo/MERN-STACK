477a7dcf296d13a494db3f64061957bc71b37768
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2021-11-09 13:01:29 -0500

    Feat: Mostrar el formulario de tareas, al seleccionar un proyecto

--- Frontend/src/Components/tareas/FormTarea.jsx
+++ Frontend/src/Components/tareas/FormTarea.jsx
@@ -1,6 +1,18 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import ProyectoContext from '../../context/proyectos/proyectoContext';
 
 const FormTarea = () => {
+
+    // Extraer si un proyecto esta activo
+    const proyectosContext = useContext(ProyectoContext)
+    const { proyecto } = proyectosContext
+
+    // Si no hay proyecto seleccionado
+    if(!proyecto) return null
+
+    // Array destructuring para extraer el proyecto actual
+    const [ proyectoActual] = proyecto;
+
     return ( 
         <div className="formulario">
             <form action="">
