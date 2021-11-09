ccd5b7f5c2a808b82ae330d92485fe94ac018eed
Sergio Villagomez Ortega <martin.v.ortega@gmail.com>
2021-11-08 22:53:12 -0500

    Feat: Creando el provider del proyecto

--- Frontend/src/Components/proyect/proyectos.jsx
+++ Frontend/src/Components/proyect/proyectos.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import Sidebar from '../layout/Sidebar';
 import Barra from '../layout/Barra';
-import FormTarea from '../../tareas/FormTarea';
-import ListadoTareas from '../../tareas/ListadoTareas';
+import FormTarea from '../tareas/FormTarea';
+import ListadoTareas from '../tareas/ListadoTareas';
 const Proyectos = () => {
     return (
         <div className="contenedor-app">
---
+++ Frontend/src/context/proyectos/proyectoState.jsx
@@ -0,0 +1,25 @@
+import React, { useReducer } from 'react';
+
+import ProyectoContext from './proyectoContext';
+import proyectoReducer from './proyectoReducer';
+
+const ProyectoState = props => {
+
+    const initialState = {
+        formulario: false
+    }
+
+    // Dispath para ejecutar las acciones 
+    const [state, dispatch] = useReducer(proyectoReducer, initialState)
+
+    // Serie de funciones para el CRUD
+    return (
+        <ProyectoContext.Provider value={{
+            formulario: state.formulario
+        }}>
+            {props.children}
+        </ProyectoContext.Provider>
+    )
+}
+ 
+export default ProyectoState;
